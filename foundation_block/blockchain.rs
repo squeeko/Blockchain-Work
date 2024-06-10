530118c12a089183543502bea1b17d9cb400bf46
squeeko <squeeko@users.noreply.github.com>
2024-06-10 15:47:28 -0700

    blkchain features

--- foundation_block/blockchain.rs
+++
@@ -1,9 +0,0 @@
-pub fn create_blockchain(genesis_address: &str) -> Blockchain {
-    let db = sled::open(current_dir().unwrap().join("data")).unwrap();
-    let blocks_tree = db.open_tree(BLOCKS_TREE).unwrap();
-    let data = blocks_tree.get(TIP_BLOCK_HASH_KEY).unwrap();
-    let tip_hash;
-    if data.is_none() {
-        let coinbase_tx = Transaction::new_coinbase_tx(genesis_address);
-    }
-}
