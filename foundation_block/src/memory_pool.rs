c8c876372d46698a33ae8c8d3cc859da3828f689
squeeko <squeeko@users.noreply.github.com>
2024-06-11 13:14:13 -0700

    completed MemPool

--- foundation_block/src/memory_pool.rs
+++ foundation_block/src/memory_pool.rs
@@ -43,3 +43,46 @@ impl MemoryPool {
         self.inner.read().unwrap().len()
     }
 }
+
+pub struct BlockInTransit {
+    inner: RwLock<Vec<Vec<u8>>>,
+}
+
+impl BlockInTransit {
+    pub fn new() -> BlockInTransit {
+        BlockInTransit {
+            inner: RwLock::new(vec![]),
+        }
+    }
+
+    pub fn add_blocks(&self, blocks: &[Vec<u8>]) {
+        let mut inner = self.inner.write().unwrap();
+        for hash in blocks {
+            inner.push(hash.to_vec());
+        }
+    }
+
+    pub fn first(&self) -> Option<Vec<u8>> {
+        let inner = self.inner.read().unwrap();
+        if let Some(block_hash) = inner.first() {
+            return Some(block_hash.to_vec());
+        }
+        None
+    }
+
+    pub fn remove(&self, block_hash: &[u8]) {
+        let mut inner = self.inner.write().unwrap();
+        if let Some(idx) = inner.iter().position(|x| x.eq(block_hash)) {
+            inner.remove(idx);
+        }
+    }
+
+    pub fn clear(&self) {
+        let mut inner = self.inner.write().unwrap();
+        inner.clear();
+    }
+
+    pub fn len(&self) -> usize {
+        self.inner.read().unwrap().len()
+    }
+}
