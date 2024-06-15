7b4bc23c30572770b21dbabc47700b69649f315e
squeeko <squeeko@users.noreply.github.com>
2024-06-14 19:11:08 -0700

    Hardhat work

---
+++ Hardhat/hardhat.config.js
@@ -0,0 +1,18 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-ethers");
+require("@nomicfoundation/hardhat-ignition")
+/** @type import('hardhat/config').HardhatUserConfig */
+
+
+const { vars } = require("hardhat/config");
+const INFURA_API_KEY = vars.get("INFURA_API_KEY");
+const SEPOLIA_PRIVATE_KEY = vars.get("SEPOLIA_PRIVATE_KEY");
+module.exports = {
+  solidity: "0.8.24",
+  networks: {
+    sepolia: {
+      url: `https://sepolia.infura.io./v3/${INFURA_API_KEY}`,
+      accounts: [SEPOLIA_PRIVATE_KEY],
+    },
+  },
+};
